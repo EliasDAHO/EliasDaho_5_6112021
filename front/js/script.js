b6e3e6d64cf551195d0822ec86d0871216b8fa99
EliasDAHO <87639332+EliasDAHO@users.noreply.github.com>
2021-11-09 07:17:29 +0100

    insertion script

---
+++ front/js/script.js
@@ -0,0 +1,14 @@
+ for (let jsonProduct of jsonListProduct){
+    
+    let product = new Product(jsonProduct);
+    document.querySelector('#items').innerHTML += `<a href="./product.html?id=${product._id}">
+                                                    <article>
+                                                      <img id='img' src="${product.imageUrl}" alt="${product.altTxt}">
+                                                      <h3 id="nomproduit" class="productName">${product.name}</h3>
+                                                      <p class="productDescription">${product.description}</p>
+                                                    </article>
+                                                  </a> `;
+  
+  }
+
+;
